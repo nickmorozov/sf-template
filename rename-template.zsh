78c27b238612848cc79ed6a3e8e21f0de81759cf
Nick Morozov <neurodeep@gmail.com>
2025-04-08 16:31:51 -0700

    add extra module to be able to choose the target dir

--- rename-template.zsh
+++ rename-template.zsh
@@ -2,15 +2,19 @@
 
 # Ensure a variable is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <replacement-text>"
+  echo "Usage: $0 <project-name> [<module-name>]"
   exit 1
 fi
 
-# Assign the provided variable
-replacement="$1"
+# Assign the provided variable, lowercase it
+replacement="${(L)1}"
 
-# Create underscore version by replacing dashes with underscores
-replacement_underscore="${replacement//-/_}"
+# Check if a second argument is provided for module name
+if [ -n "$2" ]; then
+  module_name="$2"
+else
+  module_name="{$replacement}-module"
+fi
 
 # Get the script's root directory
 root_dir="$(cd "$(dirname "$0")" && pwd)"
@@ -23,16 +27,22 @@ mv "${root_dir}/.idea/sf-template.iml" "${root_dir}/.idea/${replacement}.iml"
 
 # Recursively replace all mentions of "sf-template" with the variable
 find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf-template/${replacement}/g" {} \;
-find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf_template/${replacement_underscore}/g" {} \;
+find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf_template/${replacement//-/_}/g" {} \;
 
-echo "Replacement completed."
+# Rename the module directory
+find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/new-module/${module_name}/g" {} \;
+mv "src/main/new-module" "src/main/${module_name}"
+
+# Remove temporary files - we only needed folders
+rm -f "src/main/default/.gitkeep"
+rm -f "src/main/new-module/.gitkeep"
+
+# Report the changes
+echo "Project renamed to ${replacement}, module renamed to ${module_name}"
 
 # Remove the script and commit changes
-
 rm -f "${script_name}"
 
 git add -A
-
-git commit -m "init ${replacement}"
-
+git commit -m "Init ${replacement}"
 git push
