1bde6133b6853362277fb03999818d521330d08f
Nick Morozov <neurodeep@gmail.com>
2025-05-13 14:11:39 -0700

    add slds linter

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 // lint-staged.config.js
 module.exports = {
-    "**/*.{cls,trigger,page,component}": (filenames) => `sf scanner run -f table -s 3 -t ${filenames.join(", ")}`
+    '**/*.{cls,trigger,page,component}': (filenames) => `sf scanner run -f table -s 3 -t ${filenames.join(', ')}`,
 };
