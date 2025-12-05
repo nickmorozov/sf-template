0bd9324b750b7f9c2ae332a785828355c9900b58
Nick Morozov <neurodeep@gmail.com>
2025-12-04 17:56:14 -0800

    refactor init script to improve variable naming and usage instructions

--- init.zsh
+++ init.zsh
@@ -2,18 +2,19 @@
 
 # Ensure a variable is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <project-name> [<module-name>]"
+  echo "Usage: $0 <name> [<feature>]"
   exit 1
 fi
 
-# Assign the provided variable, lowercase it
-replacement="${(L)1}"
+# Assign the provided variable, lowercase it. get rid of everything if URL
+domain=$(echo "${(L)1}" | sed -E 's|https://([^.]+).*|\1|')
+name=$(echo "$domain" | sed -E 's/(.*)--/\1/') # foo--bar -> foo
 
 # Check if a second argument is provided for module name
 if [ -n "$2" ]; then
-  module_name="$2"
+  feature="$2"
 else
-  module_name="{$replacement}-module"
+  feature="{$name}-module"
 fi
 
 # Get the script's root directory
@@ -23,21 +24,39 @@ root_dir="$(cd "$(dirname "$0")" && pwd)"
 script_name="$(basename "$0")"
 
 # Rename project .iml
-mv "${root_dir}/.idea/sf-template.iml" "${root_dir}/.idea/${replacement}.iml"
+mv "${root_dir}/.idea/sf-template.iml" "${root_dir}/.idea/${name}.iml"
 
 # Recursively replace all mentions of "sf-template" with the variable
-find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf-template/${replacement}/g" {} \;
-find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf_template/${replacement//-/_}/g" {} \;
+find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf-template/${name}/g" {} \;
+find "${root_dir}" -type d -name '.git' -prune -o  -type f -not -name "${script_name}" -exec sed -i '' -e "s/sf_template/${name//-/_}/g" {} \;
 
 # Create the module directory
-mkdir "src/main/${module_name}"
+mkdir "src/main/${feature}"
 
 # Report the changes
-echo "Project renamed to ${replacement}, module created as ${module_name}"
+echo "Project renamed to ${name}, created ${feature}"
 
 # Remove the script and commit changes
 rm -f "${script_name}"
 
 git add -A
-git commit -m "Init ${replacement}"
+git commit -m "Init ${name}"
 git push
+
+echo "Org Authentication..."
+
+url="https://"
+alias="${domain//--/-}"
+
+if [[ $domain =~ '--' ]]; then
+  # sf org login web --instance-url https://foo--bar.sandbox.my.salesforce.com --alias foo-bar --set-default
+  url+="$domain.sandbox"
+elif [[ $domain =~ 'dev-ed' ]]; then
+  # sf org login web --instance-url https://foo-dev-ed.develop.my.salesforce.com --alias foo-dev-ed --set-default
+else
+  # sf org login web --instance-url https://foo.my.salesforce.com --alias foo-prod --set-default
+  url+="$domain"
+  alias="${domain}-prod"
+fi
+
+sf org login web --instance-url $url.my.salesforce.com --alias $alias --set-default
